625b33bede533c68e4cc9707b9c5cbaaf062180d
Andrei Dzebysh <presnia@yahoo.com>
2021-03-13 21:33:21 +0300

    The cards are created and activated )

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,6 +1,6 @@
 import s from './style.module.css';
 
-const Layout = ({title, descr, urlBg, colorBg}) => {
+const Layout = ({ id, title, urlBg, colorBg, children }) => {
   const sectionStyle = {};
 
   if (urlBg) {
@@ -13,15 +13,18 @@ const Layout = ({title, descr, urlBg, colorBg}) => {
 
   return (
     <section className={s.root}
-             style={sectionStyle}>
-      <div className={s.wrapper}>
+             id={id}
+             style={sectionStyle}
+    >
+      <div className={s.wrapper}
+      >
         <article>
           <div className={s.title}>
             <h3>{title}</h3>
             <span className={s.separator}></span>
           </div>
           <div className={`${s.desc} ${s.full}`}>
-            <p>{descr}</p>
+            {children}
           </div>
         </article>
       </div>
