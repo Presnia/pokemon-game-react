0bc2a91cc5aae8e1ce687f16116df0789d8633ae
Andrei Dzebysh <presnia@yahoo.com>
2021-03-20 14:31:25 +0300

    A bit adjusted

--- src/routes/GamePage/index.js
+++ src/routes/GamePage/index.js
@@ -1,10 +1,11 @@
 import { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import s from './style.module.css';
 import database from "../../service/firebase";
 import Button from "../../components/Button";
 import cn from 'classnames';
 import PokemonCard from "../../components/PokemonCard";
+
+import s from './style.module.css';
 
 const GamePage = ({ isActive }) => {
   const[pokemons, setPokemons] = useState({});
@@ -15,11 +16,11 @@ const GamePage = ({ isActive }) => {
     })
   }, []);
 
-  const handleClickOnCards = () => {
+  const handleClickOnCards = (id) => {
     setPokemons(prevState => {
       return Object.entries(prevState).reduce((acc, item) => {
         const pokemon = {...item[1]};
-        if (pokemon.id) {
+        if (pokemon.id === id) {
           pokemon.active = true;
         };
 
@@ -33,7 +34,8 @@ const GamePage = ({ isActive }) => {
   const history = useHistory();
   const handleClick = () => {
     history.push('/');
-  }
+  };
+
   return (
     <>
       <div className={s.div}>
