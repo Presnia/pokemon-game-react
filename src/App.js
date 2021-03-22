98a53bb40bbbdac5bdd0e04cf8ec9cf33069d835
Andrei Dzebysh <presnia@yahoo.com>
2021-03-22 14:16:39 +0300

    The file firebaseContext is created
    The file firebaseContext is created and deleted all extra files used in test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useRouteMatch, Route, Switch, Redirect } from "react-router-dom";
-import MenuHeader from "./components/MenuHeader";
 
 import cn from 'classnames';
 
@@ -7,38 +6,45 @@ import HomePage from "./routes/HomePage";
 import GamePage from "./routes/GamePage";
 import AboutPage from "./routes/AboutPage";
 import ContactPage from "./routes/ContactPage";
+import MenuHeader from "./components/MenuHeader";
 import NotFoundPage from "./routes/NotFound";
 import Footer from "./components/Footer";
 
+import {FireBaseContext} from "./context/firebaseContext";
+import Firebase from "./service/firebase";
+
 import s from './App.module.css';
+
 
 const App = () => {
   const match = useRouteMatch('/');
 
   return (
-      <Switch>
-        <Route path="/notfound" component={NotFoundPage}/>
-        <Route>
-          <>
-            <MenuHeader bgActive={!match.isExact}/>
-            <div className={cn(s.wrap, {
-              [s.isHomePage]: match.isExact
-            })}>
-              <Switch>
-                <Route path="/" exact component={HomePage}/>
-                <Route path="/home" component={HomePage}/>
-                <Route path="/game" component={GamePage}/>
-                <Route path="/about" component={AboutPage} />
-                <Route path="/contact" component={ContactPage}/>
-                <Route render={() => (
-                  <Redirect to="/notfound"/>
-                )}/>
-              </Switch>
-            </div>
-            <Footer />
-          </>
-        </Route>
-      </Switch>
+      <FireBaseContext.Provider value={new Firebase()}>
+        <Switch>
+          <Route path="/notfound" component={NotFoundPage}/>
+          <Route>
+            <>
+              <MenuHeader bgActive={!match.isExact}/>
+              <div className={cn(s.wrap, {
+                [s.isHomePage]: match.isExact
+              })}>
+                <Switch>
+                  <Route path="/" exact component={HomePage}/>
+                  <Route path="/home" component={HomePage}/>
+                  <Route path="/game" component={GamePage}/>
+                  <Route path="/about" component={AboutPage} />
+                  <Route path="/contact" component={ContactPage}/>
+                  <Route render={() => (
+                    <Redirect to="/notfound"/>
+                  )}/>
+                </Switch>
+              </div>
+              <Footer />
+            </>
+          </Route>
+        </Switch>
+      </FireBaseContext.Provider>
   )
 };
 
--- src/routes/AboutPage/index.js
+++ src/routes/AboutPage/index.js
@@ -1,9 +1,13 @@
 import './style.module.css';
 
 const AboutPage = () => {
+
   return (
     <div className="about">
       <h1>Page "About"</h1>
+      <button>
+        Change Theme
+      </button>
     </div>
   );
 };
