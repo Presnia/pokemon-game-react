053bfc3c681024b718fccb5180ee6565961f791a
Andrei Dzebysh <presnia@yahoo.com>
2021-03-25 22:15:03 +0300

    A bit corrected

--- src/routes/GamePage/routes/Board/index.js
+++ src/routes/GamePage/routes/Board/index.js
@@ -27,13 +27,13 @@ const counterWin = (board, player1, player2) => {
   const BoardPage = () => {
     const { pokemons } = useContext(PokemonContext);
     const [board, setBoard] = useState([]);
+    const [player2, setPlayer2] = useState([]);
     const [player1, setPlayer1] = useState(() => {
       return Object.values(pokemons).map(item => ({
         ...item,
         possession: 'blue'
       }))
     });
-    const [player2, setPlayer2] = useState([]);
     const [choiceCard, setChoiceCard] = useState(null);
     const [steps, setSteps] = useState(0);
 
@@ -41,16 +41,16 @@ const counterWin = (board, player1, player2) => {
 
 
   useEffect(async () => {
-    const boardResponse = await fetch('https://reactmarathon-api.netlify.app/api/board');
-    const boardRequest = await boardResponse.json();
+    const boardRequest = await fetch('https://reactmarathon-api.netlify.app/api/board');
+    const boardResponse = await boardRequest.json();
 
-    setBoard(boardRequest.data);
+    setBoard(boardResponse.data);
 
-    const player2Response = await fetch('https://reactmarathon-api.netlify.app/api/create-player');
-    const player2Request = await player2Response.json();
+    const player2Request = await fetch('https://reactmarathon-api.netlify.app/api/create-player');
+    const player2Response = await player2Request.json();
 
     setPlayer2(() => {
-      return player2Request.data.map(item => ({
+      return player2Response.data.map(item => ({
         ...item,
         possession: 'red'
       }))
@@ -84,7 +84,7 @@ const counterWin = (board, player1, player2) => {
       }
 
       if (choiceCard.player === 2) {
-        setPlayer1(prevState => prevState.filter(item => item.id !== choiceCard.id))
+        setPlayer2(prevState => prevState.filter(item => item.id !== choiceCard.id))
       }
 
       setBoard(request.data);
@@ -103,13 +103,13 @@ const counterWin = (board, player1, player2) => {
           const [count1, count2] = counterWin(board, player1, player2);
 
           if (count1 > count2) {
-            setResult('win');
+            setResult(() => 'win');
             setActive(!isActive);
           } else if (count1 < count2) {
-            setResult('lose');
+            setResult(() => 'lose');
             setActive(!isActive);
           } else {
-            setResult('draw');
+            setResult(() => 'draw');
             setActive(!isActive);
           }
       }
