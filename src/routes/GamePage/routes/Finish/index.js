c1ad58cea2fa985460b8aaf5affbb49ec916ac6a
Andrei Dzebysh <presnia@yahoo.com>
2021-03-26 10:06:57 +0300

    Added the possibility to get pokemon card if you win but with a bug yet

--- src/routes/GamePage/routes/Finish/index.js
+++ src/routes/GamePage/routes/Finish/index.js
@@ -1,19 +1,28 @@
 import { useHistory } from 'react-router-dom';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { PokemonContext } from "../../../../context/pokemonContext";
+import { FireBaseContext } from "../../../../context/firebaseContext";
 import { BoardContext } from "../../../../context/boardContext";
 import s from './style.module.css';
 import PokemonCard from "../../../../components/PokemonCard";
 
 const FinishPage = () => {
+  const { pokemons } = useContext(PokemonContext);
+  const firebase = useContext(FireBaseContext);
+  const [statePokemon, setStatePokemon] = useState({});
   const history = useHistory();
+
+  const checkCards = (item) => {
+    const player2Cards = {
+      ...item
+    }
+    setStatePokemon(player2Cards);
+  }
 
   const handleClick = () => {
     history.replace('/game');
+    firebase.addPokemon(statePokemon)
   }
-
-  const { pokemons } = useContext(PokemonContext);
-
 
   const value = useContext(BoardContext);
   console.log('===> value', value)
@@ -39,17 +48,19 @@ const FinishPage = () => {
         >
           END GAME
         </button>
+
         <section className={s.player2}>
           {
             Object.values(pokemons).map(item => (
-              <PokemonCard key={item.key}
-                           type={item.type}
-                           name={item.name}
-                           img={item.img}
-                           id={item.id}
-                           values={item.values}
-                           isActive
-              />
+                <PokemonCard key={item.key}
+                             type={item.type}
+                             name={item.name}
+                             img={item.img}
+                             id={item.id}
+                             values={item.values}
+                             isActive
+                             onClick={() => checkCards(item.id, item)}
+                />
             ))
           }
         </section>
